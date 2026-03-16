49c01c3b3ec1e4ff56fc1b41a532b1d997e7c7a7
ceba <ceba_robot@outlook.com>
2026-03-16 19:48:21 +0800

    fix: solve warning of fputcsv() jcc
    Signed-off-by: ceba <ceba_robot@outlook.com>

--- application/controllers/Awards.php
+++ application/controllers/Awards.php
@@ -463,9 +463,9 @@ class Awards extends CI_Controller {
 
 		$fp = fopen( 'php://output', 'w' );
 		$i=1;
-		fputcsv($fp, array('No', 'Callsign', 'Date', 'Band', 'Mode', 'Remarks'), ',');
+		fputcsv($fp, array('No', 'Callsign', 'Date', 'Band', 'Mode', 'Remarks'), escape: '\\');
 		foreach ($qsos as $qso) {
-			fputcsv($fp, array($i, $qso['call'], $qso['date'], ($qso['prop_mode'] != null ? $qso['band'].' / '.$qso['prop_mode'] : $qso['band']), $qso['mode'], $qso['cnty'].' - '.$qso['jcc']), ',');
+			fputcsv($fp, array($i, $qso['call'], $qso['date'], ($qso['prop_mode'] != null ? $qso['band'].' / '.$qso['prop_mode'] : $qso['band']), $qso['mode'], $qso['cnty'].' - '.$qso['jcc']), escape: '\\');
 			$i++;
 		}
 		fclose($fp);
